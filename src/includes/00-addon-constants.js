7d8d02cb35158eba241d7dd3cdbe9587f7288c63
xabolcs <szab.hu@gmail.com>
2011-06-28 05:02:48 +0200

    cleanup in addon-constants
    --HG--
    branch : duplicate2window-default

--- src/includes/00-addon-constants.js
+++ src/includes/00-addon-constants.js
@@ -4,12 +4,3 @@ const keyID = "DTW:NewWin";
 const fileMenuitemID = "menu_FileDuplicateToWindowItem";
 const addonID = "duplicate2window@szabolcs.hubai";
 
-// import
-//var EXPORTED_SYMBOLS  = ['addonConstants'];
-//
-//var addonConstants = {
-//   keyset : "duplicate-2-window-keyset"
-//  , key : "DTW:NewWin"
-//  , fileMenuitem : "menu_FileDuplicateToWindowItem"
-//  , addon : "duplicate2window@szabolcs.hubai"
-//};
